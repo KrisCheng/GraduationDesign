f6b8ac4148d8b46a5d99c3f77faaf90f50f2881f
KrisCheng <743628145@qq.com>
2017-03-22 10:21:19 +0800

    1.test

---
+++ March/CTS-SS/main.f95
@@ -0,0 +1,20 @@
+! A fortran95 program for G95
+! By Kris Chan
+program main
+  implicit none
+  integer re_i
+  integer :: iteTime = 10,i
+  real ,dimension(10) :: initNumber
+  real ,dimension(10) :: initCandidate
+  real :: pi = 3.1415926
+  call random_seed ()
+  !generate 10 numbers randomly
+  do i=1,iteTime,1
+    call random_number(initNumber(i))
+    initCandidate(i) = -1+2*sin(pi*initNumber(i))
+    write(*,*) initCandidate(i)
+  end do
+  !choose the best initial number in the 10 numbers
+
+  re_i = system("pause")
+end
