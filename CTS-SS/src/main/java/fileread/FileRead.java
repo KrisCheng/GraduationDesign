69bcddac8c9172333fd2be8bfacb5958245de49f
Kris_Chan <743628145@qq.com>
2017-03-23 16:49:20 +0800

    1.get the data from file;

--- CTS-SS/src/main/java/fileread/FileRead.java
+++ CTS-SS/src/main/java/fileread/FileRead.java
@@ -1,6 +1,7 @@
 package fileread;
 
 import ucar.ma2.Array;
+import ucar.ma2.Index;
 import ucar.nc2.NCdumpW;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
@@ -11,23 +12,29 @@ import java.io.IOException;
  */
 public class FileRead {
     public static void main(String[] args) {
-        String filename = "src/main/java/fileread/ocean_temp_salt.res.nc";
+        //获取nc文件中x轴和y轴数据
+        final String filename = "src/main/java/fileread/ocean_temp_salt.res.nc";
+        final int xAxis = 360;
+        final int yAxis = 200;
+        double[][] data = new double[xAxis][yAxis];
         NetcdfFile ncfile = null;
         try {
             ncfile = NetcdfFile.open(filename);
-            //find variable
+            //共有 temp 和 salt 两种参数
             String variable = "temp";
             Variable varBean = ncfile.findVariable(variable);
-            //Reading data from a Variable
-            if(null != varBean) {
-                Array all = varBean.read();
-                System.out.println("读取所有：\n"+NCdumpW.printArray(all, variable, null));
-            }
+            //读取数据
             if(null != varBean) {
                 int[] origin = new int[] {0,0,0,0};
-                int[] size = new int[] {1,1,2,2};
-                Array data2D = varBean.read(origin, size);
-                System.out.println(NCdumpW.printArray(data2D, variable, null));
+                int[] size = new int[] {1,1,yAxis,xAxis};
+                Array data2D = varBean.read(origin, size).reduce();
+                Index index = data2D.getIndex();
+                //将数据存储至data数组中
+                for(int i = 0; i < yAxis; i++){
+                    for(int j = 0; j < xAxis; j++){
+                        data[j][i] = data2D.getDouble(index.set(i,j));
+                    }
+                }
             }
         } catch (Exception ioe) {
             ioe.printStackTrace();
