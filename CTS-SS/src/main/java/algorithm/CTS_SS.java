33884361a3086fd2432d92bced1bf6626036b2f4
Kris_Chan <743628145@qq.com>
2017-03-28 21:37:21 +0800

    1.finish the basic structure of the algorithm implement;

--- CTS-SS/src/main/java/algorithm/CTS_SS.java
+++ CTS-SS/src/main/java/algorithm/CTS_SS.java
@@ -1,11 +1,12 @@
 package algorithm;
 
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by Kris Chan on 9:39 AM 24/03/2017 .
  * All right reserved.
- * The main class for cts-ss algorithm.
+ * The main class for CTS-SS algorithm.
  */
 public class CTS_SS {
     //初始参数配置
@@ -18,17 +19,19 @@ public class CTS_SS {
     private int R2; //阶段1邻域半径
     private int MAX_CYCLE1; //阶段一迭代次数
     private int MAX_CYCLE2; //阶段二迭代次数
-    private float tabuParameter; //禁忌判定参数
-    private float[][] tabuList = new float[Dim][tabuLength]; //禁忌表
-    private float[][] initList = new float[Dim][initNumber]; //候选初始解列表
-    private float[] initValue = new float[Dim]; //初始解
-    private float initEvaluation; //初始解扰动值
-    private float[] bestValue = new float[Dim]; //当前最优解
-    private float bestEvaluation; //最优解扰动值
-    private float[] tempValue = new float[Dim]; //临时解
-    private float tempEvaluation; //临时解扰动值
-    private float[] localValue = new float[Dim]; //一次迭代中的局部解
-    private float localEvaluation; //局部解扰动值
+    private double tabuParameter; //禁忌判定参数
+    private double[][] tabuList = new double[tabuLength][Dim]; //禁忌表
+    private double[][] initList; //候选初始解列表
+    private double[] initValue = new double[Dim]; //初始解
+    private double initEvaluation; //初始解扰动值
+    private double[] bestValue; //当前最优解
+    private double bestEvaluation; //最优解扰动值
+    private double[] tempValue; //临时解
+    private double tempEvaluation; //临时解扰动值
+    private double[] localValue; //一次迭代中的局部解
+    private double localEvaluation; //局部解扰动值
+    Random random = new Random(); //生成随机数
+    private int curCycle; //当前迭代次数
 
     public CTS_SS(int s1, int r1, int s2, int r2, float para){
         m1 = s1;
@@ -48,10 +51,94 @@ public class CTS_SS {
     }
 
     //生成Sine初值
+     public void initSineMap(){
+        initList = new double[Dim][initNumber];
+        for(int i = 0; i < initNumber; i++){
+            for(int j = 0; j < Dim; j++){
+                initList[i][j] = Math.sin(Math.PI * Math.random());
+            }
+            //todo:调用shell模式求解,在满足约束的条件(即禁忌判断参数)下,与适应度函数做对比,得到最优初始解
+        }
+    }
 
 
-    //Stage1
+    //获取CNOP值
+    public double evaluate(double[] chr){
+        double CNOP = 0;
+        for(int i = 0; i < Dim; i++){
+            //todo:此处做矩阵减法,通过适应度函数来获取CNOP
+        }
+        return CNOP;
+    }
 
-    //Stage2
+    //领域交换
+    public void swap(int radius, double[] tempChr){
+        boolean flag = true;
+        while(flag) {
+            for(int i = 0; i < radius; i++) {
+                int rand =random.nextInt()%Dim;
+                tempChr[rand] = Math.sin(Math.PI * Math.random());
+            }
+            //todo:禁忌参数判断,若满足条件,则领域交换成功,退出
+            if(true){
+                flag = false;
+            }
+        }
+    }
 
+    //判断某个解是否在禁忌表中
+    public boolean isTabuList(double[] tempChr){
+        for(int i = 0; i < tabuLength; i++){
+            //todo:tempChr与tabuList中元素进行对比
+            if(true){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //解除禁忌与加入禁忌表
+    public void addtoTabuList(double[] tempChr){
+        for(int i = 0; i < tabuLength-1; i++){
+            for(int j = 0; j < Dim; j++){
+                tabuList[i][j] = tabuList[i+1][j];
+            }
+        }
+        //将新的最优解加入禁忌表
+        for(int k = 0; k < Dim; k++){
+            tabuList[tabuLength-1][k] = tempChr[k];
+        }
+    }
+
+    //初始并分阶段搜索
+    public void StagedSearch() {
+        //1.初始化
+        bestValue = new double[Dim];
+        tempValue = new double[Dim];
+        localValue = new double[Dim];
+        initSineMap();
+        //2.分阶段搜索
+        for(curCycle = 1; curCycle <= MAX_CYCLE1; curCycle++) {
+            for (int i = 0; i < m1; i++) {
+                swap(R1, tempValue);
+                if (isTabuList(tempValue)) {
+                    addtoTabuList(tempValue);
+                }
+            }
+        }
+        for(curCycle = 1; curCycle <= MAX_CYCLE2; curCycle++) {
+            for (int i = 0; i < m2; i++) {
+                swap(R2, tempValue);
+                if (isTabuList(tempValue)) {
+                    addtoTabuList(tempValue);
+                }
+            }
+        }
+        //3.打印结果
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
